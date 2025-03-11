87f4caa2b738996c003083a5afadd01826c74262
heeralall <saman@baobab-ingenierie.fr>
2025-03-11 09:34:46 +0100

    Validators

--- apps/account/views.py
+++ apps/account/views.py
@@ -5,7 +5,10 @@ from apps.account.forms.register_forms import RegisterForm
 
 # Create your views here.
 def register(request):
-    form = RegisterForm()
+    form = RegisterForm(request.POST or None)
+    if form.is_valid():
+        #Traitement si le formulaire est valide
+        print("Formulaire validé", form.cleaned_data)
     return render(request, 'account/register.html', {
         'form': form
     })
